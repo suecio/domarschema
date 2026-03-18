39de828e2b4877821d17e3bf728f9dd2b9fccc58
suecio <73824448+suecio@users.noreply.github.com>
2026-03-18 12:05:28 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css' // This line MUST be here to load the styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
